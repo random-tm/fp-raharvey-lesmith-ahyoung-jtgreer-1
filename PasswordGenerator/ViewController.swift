177c41a1616ab69880614165a53cdf66436447db
Rachel Harvey <raharvey@bsu.edu>
2015-10-19 20:25:07 -0400

    Initial Commit

---
+++ PasswordGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PasswordGenerator
+//
+//  Created by admin on 10/19/15.
+//  Copyright © 2015 Rachel Harvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
