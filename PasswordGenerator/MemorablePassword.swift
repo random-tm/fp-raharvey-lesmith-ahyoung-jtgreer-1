5fc8844084d4a5ce6e6b921e6c10ff2e1d5d6dd1
Lucas Smith <friedmicrochips@gmail.com>
2015-10-26 19:32:48 -0400

    Only return words which do not have a space.

--- PasswordGenerator/MemorablePassword.swift
+++ PasswordGenerator/MemorablePassword.swift
@@ -21,11 +21,27 @@ class MemorablePassword {
     }
 
     func getRandomWord() -> String{
-        let word = self.randomWordGeneratorOne.getRandomWord()
+        let word = getWordWithoutSpaces(randomWordGeneratorOne)
         self.randomWordGeneratorTwo = self.initRandomWordWithLengthDifference(word)
-        let word2 = self.randomWordGeneratorTwo.getRandomWord()
+        let word2 = getWordWithoutSpaces(randomWordGeneratorTwo)
         self.password = word + word2
         return self.password
+    }
+    
+    func getWordWithoutSpaces(randomWordGenerator:RandomWord) -> String{
+        var string = " "
+        while(checkForSpaces(string)){
+            string = randomWordGenerator.getRandomWord()
+        }
+        return string
+    }
+    
+    func checkForSpaces(string:String) -> Bool{
+        if(string.containsString(" ")){
+            return true
+        } else {
+            return false
+        }
     }
     
     func initRandomWordWithLengthDifference(word: String) -> RandomWord{
