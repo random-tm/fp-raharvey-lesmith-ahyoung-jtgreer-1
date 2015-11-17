3d95ae51368464a00ecd16d52676dc70f2a4757d
Lucas Smith <friedmicrochips@gmail.com>
2015-11-16 20:48:45 -0500

    Generate a SHA hash to function as a password from the current time and date of the device.

---
+++ PasswordGenerator/CryptoswiftWrapper.swift
@@ -0,0 +1,39 @@
+//
+//  CryptoswiftWrapper.swift
+//  PasswordGenerator
+//
+//  Created by Lucas Smith on 11/16/15.
+//  Copyright © 2015 Rachel Harvey. All rights reserved.
+//
+
+import Foundation
+import CryptoSwift
+
+class CryptoswiftWrapper{
+    
+    private let length:Int
+    
+    init(length : Int){
+        self.length = length
+    }
+    
+    func getPassword() -> String{
+        let rawData = getDate()
+        let password = rawData.sha512()
+        return trimPassword(password)
+    }
+    
+    private func getDate() -> String{
+        let date = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.timeStyle = .FullStyle
+        let data = formatter.stringFromDate(date)
+        return data
+    }
+    
+    private func trimPassword(password:String) -> String{
+        let trimIndex = password.startIndex.advancedBy(length)
+        return password.substringToIndex(trimIndex)
+    }
+    
+}
