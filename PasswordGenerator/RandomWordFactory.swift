4250c70ddfbad387a63b1b1544ba0d18fa1c9bd4
Lucas Smith <friedmicrochips@gmail.com>
2015-11-17 20:03:04 -0500

    Move error handling code in RandomWordFactory.swift to a separate function.

--- PasswordGenerator/RandomWordFactory.swift
+++ PasswordGenerator/RandomWordFactory.swift
@@ -63,14 +63,18 @@ class RandomWordFactory {
     private func createNetworkTask(semaphore:dispatch_semaphore_t) -> NSURLSessionDataTask{
         let task = NSURLSession.sharedSession().dataTaskWithRequest(self.requestHeaders) {
             data, response, error in
-            if(response != nil && self.checkResponseCode(response!)){
-                self.parseNetworkData(data)
-            } else {
-                self.word = "NetworkError"
-            }
+            self.setWord(response, data: data)
             dispatch_semaphore_signal(semaphore)
         }
         return task
+    }
+    
+    private func setWord(response:NSURLResponse?, data:NSData?) -> Void{
+        if(response != nil && self.checkResponseCode(response!)){
+            self.parseNetworkData(data)
+        } else {
+            self.word = "NetworkError"
+        }
     }
     
     private func checkResponseCode(response:NSURLResponse) -> Bool{
