7e85ee1da997a31cf44a5ab9c7b993e49c63d7e6
Rachel Harvey <raharvey@bsu.edu>
2015-12-04 14:15:27 -0500

    Clean ViewController and reattach CopyButton

--- PasswordGenerator/ViewController.swift
+++ PasswordGenerator/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
         self.labelDenotingGeneratedPassword.hidden = true
     }
     
-    @IBAction func copyButtonPushed(sender: UIButton) -> Void {
+    @IBAction func copyButtonPushed() -> Void {
         let stringToCopy = self.generatedPasswordLabel.text
         if(self.stringIsCopyable(stringToCopy!)) {
             copyPasswordToClipboard()
