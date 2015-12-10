ab2d0832eccd985f0f85f3e32e53c2729e40d89a
Lucas Smith <friedmicrochips@gmail.com>
2015-12-09 21:12:21 -0500

    Move folder groups into physical folders.

---
+++ PasswordGenerator/UI/CopyButton.swift
@@ -0,0 +1,28 @@
+//
+//  CopyButton.swift
+//  PasswordGenerator
+//
+//  Created by admin on 12/4/15.
+//  Copyright © 2015 Rachel Harvey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CopyButton: BorderedButton {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.hideButton()
+    }
+    
+    func showButton() {
+        self.enabled = true
+        self.hidden = false
+    }
+    
+    func hideButton() {
+        self.enabled = false
+        self.hidden = true
+    }
+}
