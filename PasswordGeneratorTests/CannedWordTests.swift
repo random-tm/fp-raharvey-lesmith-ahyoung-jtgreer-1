1e63e34b2457fbc8997c7d9977df9d0f3e21345f
Lucas Smith <friedmicrochips@gmail.com>
2015-12-10 19:25:34 -0500

    Test all possible password lengths involving MemorablePassword.

--- PasswordGeneratorTests/CannedWordTests.swift
+++ PasswordGeneratorTests/CannedWordTests.swift
@@ -13,12 +13,26 @@ class CannedWordTests: XCTestCase {
     
     func testCannedWordIsNotNil() {
         let wordGenerator = CannedWord()
-        XCTAssert(wordGenerator.getRandomWord(6, minLength: 6) as String! != nil)
+        for length in 12...24 {
+            XCTAssert(wordGenerator.getRandomWord(length, minLength: 1) as String! != nil)
+        }
     }
     
     func testMemorableGenerationLength() {
         let wordGenerator = CannedWord()
-        XCTAssert(wordGenerator.getRandomWord(6, minLength: 6) == "beaver")
+        for length in 12...24 {
+            let word = wordGenerator.getRandomWord(length, minLength: 1)
+            XCTAssert(isLengthOfEqualTo(word, length: length))
+        }
+    }
+    
+    private func isLengthOfEqualTo(word: String, length: Int) -> Bool{
+        let wordLength = word.characters.count
+        if(wordLength == length){
+            return true
+        } else {
+            return false
+        }
     }
     
 }
